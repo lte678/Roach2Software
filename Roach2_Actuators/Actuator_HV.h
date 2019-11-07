fde8c13048ac0e21e93b84bf91f461c31ea0306a
Marc Bamann <marc.bamann@online.de>
2019-11-07 12:26:50 +0100

    Upload Actuator_HV.h

---
+++ Roach2_Actuators/Actuator_HV.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _Actuator_HV_h_
+#define _Actuator_HV_h_
+
+#include "Actuator.h"
+
+class Actuator_HV : public Actuator
+{
+public:
+	Actuator_HV();
+	~Actuator_HV();
+
+private:
+
+protected:
+
+};
+
+#endif 
