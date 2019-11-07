71a05a5fa5a4d9f63d6abd2b531cca30492d8f0c
Marc Bamann <marc.bamann@online.de>
2019-11-07 12:24:42 +0100

    Upload Actuator_GoPro.h

---
+++ Roach2_Actuators/Actuartor_GoPro.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _Actuator_GoPro_h_
+#define _Actuator_GoPro_h_
+
+#include "Actuator.h"
+
+class Actuator_GoPro : public Actuator
+{
+public:
+	Actuator_GoPro();
+	~Actuator_GoPro();
+
+private:
+
+protected:
+
+};
+
+#endif
