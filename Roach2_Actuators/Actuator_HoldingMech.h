89e471b55d6a42e920f3edfc12d5593242a2ec28
Marc Bamann <marc.bamann@online.de>
2019-11-07 12:26:09 +0100

    Upload Actuator_HoldingMech.h

---
+++ Roach2_Actuators/Actuator_HoldingMech.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _Actuator_HoldingMech_h_
+#define _Actuator_HoldingMech_h_
+
+#include "Actuator.h"
+
+class Actuator_HoldingMech : public Actuator
+{
+public:
+	Actuator_HoldingMech();
+	~Actuator_HoldingMech();
+
+private:
+
+protected:
+
+};
+
+#endif 
